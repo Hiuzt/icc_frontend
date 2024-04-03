28a3a0116384c91a73977dcfef643fded97ed790
Hiuzt <kerner.daniel1@gmail.com>
2024-04-03 16:31:25 +0200

    backendupdate

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export default axios.create({
-    baseURL: "https://localhost:7273/api",
+    baseURL: "https://backend20240403160743.azurewebsites.net/api",
     withCredentials: true,
 })
